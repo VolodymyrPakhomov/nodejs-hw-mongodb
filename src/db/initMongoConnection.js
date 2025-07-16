e93b10a7af8997adc7beb4b4b77519d5347b5dcd
Name <vova.zest997@gmail.com>
2025-07-16 05:00:56 +0300

    HW2 MongoDB 10

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -8,7 +8,7 @@ export const initMongoConnection = async () => {
     const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } =
       process.env;
     const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
-
+    console.log('Connecting to:', connectionString);
     await mongoose.connect(connectionString);
     console.log('Mongo connection successfully established!');
   } catch (error) {
